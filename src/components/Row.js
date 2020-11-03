b8174e1011c845acd484845055f568ea7ec56e3c
Suppakorn Bas <suppakorn.resume@gmail.com>
2020-11-03 22:29:57 +0700

    create project.

---
+++ src/components/Row.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import './Row.css';
+
+function Row({ title, poster }) {
+
+  
+
+  return (
+    <div className="row">
+
+    </div>
+  )
+}
+
+export default Row
